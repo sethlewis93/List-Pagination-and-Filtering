e41be9f180c727c2f47b9de1d809a76d709e68ff
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-08-18 11:36:48 -0400

    removed if statement

--- js/script.js
+++ js/script.js
@@ -40,7 +40,6 @@ const appendPageLinks = (list) => {
     for (let j = 0; j < getAnchors.length; j++) { // for every anchor...
       liEls.addEventListener('click', (e) => { // listener works BUT IF YOU CLICK AWAY FROM LIST, BUG OCCURS!
         let page = e.target.innerHTML; // target and store the text of the element clicked
-        if (a)
           getAnchors[j].classList.remove('active');
           e.target.classList.add('active'); // ...add active class name to link clicked
         showPage(studentList, page); // page parameter corresponds to text of element clicked
