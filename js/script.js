7b86c9e648a42348c66993f47eef4af0a73e3dfb
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-08-20 07:23:42 -0400

    appendPageLink function behaving correctly
    working on adding student-search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// variable storing student LIST ITEM ELEMENTS 
+// variable storing student LIST ITEM ELEMENTS
 const studentList = document.getElementsByClassName("student-item cf"); // querySelectorAll NOT WORKING FOR SOME REASON...
 // variable storing number of items to show on page
 const pageItems = 10;
@@ -10,43 +10,55 @@ const showPage = (list, page) => {
   for (let i = 0; i < list.length; i++) {
     list[i].style.display = "none"; // hiding list items by default
     if (i >= startIndex && i < endIndex) {
-      list[i].style.display = "list-item"; 
+      list[i].style.display = "list-item";
     }
   }
 };
 
-// function to create pagination buttons
-// RESEARCH: USING LITERALS HERE 
+// Function to create pagination buttons
 const appendPageLinks = (list) => {
+
+  // Page link variables
   let pages = Math.ceil(list.length / pageItems); // # of pages needed for list
   let classDiv = document.querySelector(".page");
   let conDiv = document.createElement("div"); // created 'pagination' div
-  conDiv.className = "pagination"; 
+  conDiv.className = "pagination";
   classDiv.appendChild(conDiv); // append new div to "page" div
-  let ulElm = document.createElement("ul"); // created new ul element 
-  ulElm.className = "pagination"; 
+  let ulElm = document.createElement("ul"); // created new ul element
+  ulElm.className = "pagination";
   conDiv.appendChild(ulElm); // appended new ul to pagination div
 
-  for (let i = 0; i < pages; i++) { // for every page needed based on list size...
+  for (let i = 0; i < pages; i++) {
+    // for every page needed based on list size...
     let liEls = document.createElement("li"); // ...create new li...
     ulElm.appendChild(liEls); // ...append new li to new ul...
     let a = document.createElement("a"); // ...create anchor tag
     a.href = "#";
     a.innerHTML = i + 1; // text corresponds to # of pages needed for list
     liEls.appendChild(a);
-    const firstAnchor = document.getElementsByTagName('a')[0]; // retrieve first anchor
+    const firstAnchor = document.getElementsByTagName("a")[0]; // retrieve first anchor
     firstAnchor.className = "active"; // set first anchor (link to the page 1) to active
-    const getAnchors = document.querySelectorAll('a'); // grab all new anchor tags
-    for (let j = 0; j < getAnchors.length; j++) { // for every anchor...
-      liEls.addEventListener('click', (e) => { 
-        let page = e.target.innerHTML; // target and store the text of the element clicked
-        getAnchors[j].classList.remove('active');
-        e.target.classList.add('active'); // ...add active class name to link clicked
-        showPage(studentList, page); // page parameter corresponds to text of element clicked
-      });
-    };
-  }  
+    liEls.addEventListener("click", (e) => {
+      const getAnchors = document.querySelectorAll("a"); // grab all new anchor tags
+      for (let j = 0; j < getAnchors.length; j++) {
+        getAnchors[j].classList.remove("active");
+      }
+      let page = e.target.innerHTML; // target and store the text of the element clicked
+      e.target.classList.add("active"); // ...add active class name to link clicked
+      showPage(studentList, page); // page parameter corresponds to text of element clicked
+    });
+  }
 };
 showPage(studentList, 1);
 appendPageLinks(studentList);
+
 // add search button
+const pageHeaderDiv = document.querySelector(".page-header cf");
+console.log(pageHeaderDiv);
+
+pageHeaderDiv.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchDiv = document.createElement("div");
+  searchDiv.className = "student-search";
+  pageHeaderDiv.appendChild(searchDiv);
+});
