edab3ef398d4d3a72bbd5a9ef696e76aed80a996
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-08-28 17:53:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,8 +120,8 @@ button.addEventListener('click', (e) => { // NEW BUG: 'click' seems to be the on
 
     const h2 = createElement('h2', 'textContent', 'No results found matching your search. Please try again.');
     h2.id = 'no-results';
-    const pageDiv = document.querySelector('.page');
-    pageDiv.appendChild(h2);
+    const paginationDiv = document.querySelector('.pagination');
+    paginationDiv.appendChild(h2);
 
 
     } else {
